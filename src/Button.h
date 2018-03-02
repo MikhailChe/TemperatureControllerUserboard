8818c7d7d62bfcd5fb53dd3153ce031fd8b0da90
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2018-03-02 19:05:52 +0500

    Git ignore

--- src/Button.h
+++ src/Button.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Button.h
  *
@@ -29,34 +28,4 @@ private:
 bool isPressed(Button& but);
 bool isClicked(Button& but);
 #endif /* BUTTON_H_ */
-=======
-/*
- * Button.h
- *
- *  Created on: 10 февр. 2018 г.
- *      Author: Mikhail
- */
 
-#ifndef BUTTON_H_
-#define BUTTON_H_
-
-#include <stm32f30x.h>
-#include <stm32f30x_gpio.h>
-#include <sys/_stdint.h>
-
-#include "TimeUtils.h"
-
-class Button final {
-public:
-	Button(GPIO_TypeDef* gpio, uint16_t pinNum);
-	Button(GPIO_TypeDef* gpio, uint16_t pinNum, GPIOPuPd_TypeDef pull);
-	bool get();
-private:
-	GPIO_TypeDef* port;
-	uint8_t pin;
-	bool inverse;
-};
-bool isPressed(Button& but);
-bool isClicked(Button& but);
-#endif /* BUTTON_H_ */
->>>>>>> branch 'master' of git@github.com:MikhailChe/TemperatureControllerUserboard.git
